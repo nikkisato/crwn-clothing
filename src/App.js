46e05c9d8fba8b6f6571bca8c01512d8053fa82d
Nikki <nikki.satopdx@gmail.com>
2020-04-18 15:36:49 -0700

    router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Route, Switch, Link } from 'react-router-dom';
+
+
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+
+
+      
+
+      </Switch>
     </div>
   );
 }
