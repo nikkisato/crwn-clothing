cf08c6ef4f2a24675295cf4c34fe26e34ad03a67
Nikki <nikki.satopdx@gmail.com>
2020-04-18 17:45:15 -0700

    created header

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component'
-import { Route, Switch, Link } from 'react-router-dom';
-
-
-
+import ShopPage from './pages/shop/shop.component';
+import { Route, Switch } from 'react-router-dom';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
-
       </Switch>
     </div>
   );
