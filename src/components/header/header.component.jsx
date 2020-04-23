000dbac64701cc89b4353755e79cbc79bae31018
Nikki <nikki.satopdx@gmail.com>
2020-04-23 13:14:58 -0700

    started redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,7 @@ import './header.styles.scss';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/original.svg';
 import { auth } from '../../firebase/firebase.utils';
+import { connect } from 'react-redux';
 
 const Header = ({ currentUser }) => {
   return (
@@ -15,15 +16,15 @@ const Header = ({ currentUser }) => {
           SHOP
         </Link>
         <Link className='option' to='/contact'>
-       CONTACT
+          CONTACT
         </Link>
         {currentUser ? (
           <div className='option' onClick={() => auth.signOut()}>
-         SIGN OUT
+            SIGN OUT
           </div>
         ) : (
           <Link className='option' to='/signin'>
-        SIGN IN
+            SIGN IN
           </Link>
         )}
       </div>
@@ -31,4 +32,10 @@ const Header = ({ currentUser }) => {
   );
 };
 
-export default Header;
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
+});
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
