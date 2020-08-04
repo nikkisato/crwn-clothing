a64b6b66d64366df75d3c66fbd7e0ea7f0fd194b
Nikki <nikki.satopdx@gmail.com>
2020-08-03 17:04:00 -0700

    working on checkout page

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { createStructuredSelector } from 'reselect';
 
+import CheckoutPage from './pages/checkout/checkout.component';
+
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
@@ -41,6 +43,8 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
+
           <Route
             exact
             path='/signin'
