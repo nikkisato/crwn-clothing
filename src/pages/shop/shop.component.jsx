76c54c4b85302302fa4fa70828f9bd9f6a6149ca
Nikki <nikki.satopdx@gmail.com>
2020-08-07 11:43:26 -0700

    implement code pulls in firestore data

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -24,15 +24,18 @@ class ShopPage extends React.Component {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
 
-    this.unsubscribeFromSnapshot = collectionRef.onSnapshot(
-      async (snapshot) => {
-        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-        updateCollections(collectionsMap);
+    // firestore fetch
+// fetch('https://firestore.googleapis.com/v1/projects/crwn-clothing-3e0ec/databases/(default)/documents/collections')
+// .then(response => response.json())
 
-        this.setState({ loading: false });
-      }
-    );
+    collectionRef.get().then((snapshot) => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+      updateCollections(collectionsMap);
+
+      this.setState({ loading: false });
+    });
   }
+
 
   render() {
     const { match } = this.props;
