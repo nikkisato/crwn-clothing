2f568ae2a2dca771d5d82afba1481c253941ab6b
Nikki <nikki.satopdx@gmail.com>
2020-08-26 18:04:27 -0700

    seperating our CRA app into client folder, added express server

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
@@ -7,8 +8,23 @@ const StripeCheckoutButton = ({ price }) => {
     'pk_test_51HCdUiJp2mpcbsqkeiIlqvRQOU0Vo1eRXX8rSxmWcPS92uA0lq1UUiZX6We88sHzMGDiPvMUqEOVcTFsQWPe6Ti600Qtd3GCdD';
 
   const onToken = (token) => {
-    console.log(token);
-    alert('Payment Successful');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token: token,
+      },
+    })
+      .then((response) => {
+        alert('Successful payment');
+      })
+      .catch((error) => {
+        console.log('Payment Error: ', error);
+        alert(
+          'There was an issue with your payment! Please make sure you use the provided credit card.'
+        );
+      });
   };
   return (
     <StripeCheckout
--- server.js
+++ server.js
@@ -1,24 +1,45 @@
 const express = require('express');
-
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const path = require('path');
-const enforce = require('express-sslify');
+
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
-app.use(express.static(path.join(__dirname, 'client/build')));
+const port = process.env.PORT || 5000;
 
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+if (process.env.NODE_ENV == 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+app.listen(port, (error) => {
+  if (error) throw error;
+  console.log('Server running on port' + port);
 });
 
-// const express = require('express');
-// const path = require('path');
-// const enforce = require('express-sslify');
+app.post('/payment', (req, res) => {
+  const body = {
+    source: req.body,token.id,
+    amount: req.body.amount,
+    currency: 'usd'
+  };
+stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripErr) {
+        res.status(500).send({error: stripeErr})
+    } else {
+        res.status(200).send({success: stripeRes})
+    }
+})
 
-// const app = express();
-// app.use(enforce.HTTPS({ trustProtoHeader: true }));
-
-// app.length('/service-worker.js', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
-// });
+});
