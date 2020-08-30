327638112519de2631e465d97256019c7e60e0bd
Nikki <nikki.satopdx@gmail.com>
2020-08-30 12:47:48 -0700

    finished redux testing, need to double check on passing

--- client/src/redux/shop/shop.actions.js
+++ client/src/redux/shop/shop.actions.js
@@ -1,36 +1,36 @@
 import ShopActionTypes from './shop.types';
 
-import { firestore, convertCollectionsSnapshotToMap } from "../../firebase/firebase.utils"
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from '../../firebase/firebase.utils';
 
 export const fetchCollectionsStart = () => ({
   type: ShopActionTypes.FETCH_COLLECTIONS_START,
 });
 
-export const fetchCollectionSuccess = collectionsMap => ({
-    type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
-    payload: collectionsMap
-})
+export const fetchCollectionSuccess = (collectionsMap) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
+  payload: collectionsMap,
+});
 
-export const fetchCollectionsFailure = errorMessage => ({
-    type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
-    payload: errorMessage
-})
+export const fetchCollectionsFailure = (errorMessage) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+  payload: errorMessage,
+});
 
 export const fetchCollectionsStartAsync = () => {
-   return dispatch => {
+  return (dispatch) => {
     const collectionRef = firestore.collection('collections');
 
-    dispatch (fetchCollectionsStart())
-    // firestore fetch
-// fetch('https://firestore.googleapis.com/v1/projects/crwn-clothing-3e0ec/databases/(default)/documents/collections')
-// .then(response => response.json())
+    dispatch(fetchCollectionsStart());
 
-    collectionRef.get().then((snapshot) => {
-      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-      dispatch(fetchCollectionSuccess(collectionsMap));
-    }).catch(error => dispatch(fetchCollectionsFailure(error.message)))
-
-     
-   }
+    collectionRef
+      .get()
+      .then((snapshot) => {
+        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+        dispatch(fetchCollectionSuccess(collectionsMap));
+      })
+      .catch((error) => dispatch(fetchCollectionsFailure(error.message)));
   };
-  
+};
